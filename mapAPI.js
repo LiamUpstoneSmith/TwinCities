c068a0577783a6e72a38b12793f714e6cb5f9b5d
Liam Upstone-Smith <liamjake2002@hotmail.co.uk>
2023-02-17 13:20:14 +0000

    Working Link to webpages

--- mapAPI.js
+++ mapAPI.js
@@ -26,7 +26,7 @@ function initialize(){
                 '<div id="bodyContent">' +
                 "<p>The Royal Albert Dock is a complex of dock buildings and warehouses in Liverpool, England." + 
                 " Designed by Jesse Hartley and Philip Hardwick, it was opened in 1846.</p>" +
-                '<a href="https://github.com/LiamUpstoneSmith/TwinCities/blob/master/Liverpool/LiverpoolLocations/RoyalAlbertDock.html"><button type="button">More Info</button></a>' + 
+                '<a href="Liverpool/RoyalAlbertDock.html"><button type="button">More Info</button></a>' + 
                 "</div>" +
                 "</div>";
 
@@ -59,7 +59,7 @@ function initialize(){
                 '<div id="bodyContent">' +
                 "<p>Anfield is a football stadium Home of Liverpool F.C. It has a seating capacity of 53,394, making it the seventh largest football stadium in England." + 
                 " It was originally the home of Everton from 1884 to 1891, before they moved to Goodison Park after a dispute with the club president.</p>" +
-                '<a href="Liverpool\LiverpoolLocations\Anfield.html"><button type="button">More Info</button></a>' + 
+                '<a href="Liverpool/Anfield.html"><button type="button">More Info</button></a>' + 
                 "</div>" +
                 "</div>";
 
@@ -91,7 +91,7 @@ function initialize(){
                 '<div id="bodyContent">' +
                 "<p>The Univerity of Liverpool is a public research university based in the city of Liverpool, england. founded as a acollege in 1881, it gained its Royal" + 
                 " Charter in 1903 with theability to award degrees, and is also known to be one of the six 'red brick' civic Universities.</p>" +
-                '<a href="Liverpool\LiverpoolLocations\Uni.html"><button type="button">More Info</button></a>' + 
+                '<a href="Liverpool/Uni.html"><button type="button">More Info</button></a>' + 
                 "</div>" +
                 "</div>";
 
@@ -124,7 +124,7 @@ const safariContent =
                 '<div id="bodyContent">' +
                 "<p>Start your adventure by exploring our impressive 5-mile safari drive which is currently home to over 750 fascinating animals, from baboons to camels." + 
                 "  wildebeest to rhinos, all from the comfort of your own car. The drive through lion enclosure is a real highlight.</p>" +
-                '<a href="Liverpool\LiverpoolLocations\safari.html"><button type="button">More Info</button></a>' + 
+                '<a href="Liverpool/safari.html"><button type="button">More Info</button></a>' + 
                 "</div>" +
                 "</div>";
 
@@ -156,7 +156,7 @@ const galleryContent =
                 '<div id="bodyContent">' +
                 "<p>The Lady Lever Art Gallery is a museum founded and built by the industrialist and philanthropist William Lever, and opened in 1922. " + 
                 "  The Lady Lever Art Gallery is set in the garden village of Port Sunlight, on the Wirral and one of the National Museums Liverpool.</p>" +
-                '<a href="Liverpool\LiverpoolLocations\gallery.html"><button type="button">More Info</button></a>' + 
+                '<a href="Liverpool/gallery.html"><button type="button">More Info</button></a>' + 
                 "</div>" +
                 "</div>";
 
@@ -189,7 +189,7 @@ const parkContent =
                 '<div id="bodyContent">' +
                 "<p>A new park the size of Anfield and Goodison's pitches combined could be built in the city's north docks and form the next stage of the Liverpool Waters development." + 
                 " According to Peel L&P, Central Park would bring direct access to parts of the Leeds Liverpool Canal for the first time in a number of years, with a section running from Canning Dock toward Waterloo Dock, Princes Dock and the Three Graces. </p>" +
-                '<a href=""><button type="button">More Info</button></a>' + 
+                '<a href="Liverpool/park.html"><button type="button">More Info</button></a>' + 
                 "</div>" +
                 "</div>";
 
@@ -215,7 +215,7 @@ const parkContent =
 
 
 
-
+    // New Orleans ----------------------------------------------------------------------------------------
 
     // Jackson Square --------------------------------------------------------------------------------------
 const JSContent = 
@@ -226,7 +226,7 @@ const JSContent =
                 '<div id="bodyContent">' +
                 "<p>Jackson Square, formerly the Place d'Armes (French) or Plaza de Armas (Spanish), is a historic park in the French Quarter of New Orleans, Louisiana. It was declared" + 
                 "  a National Historic Landmark in 1960, for its central role in the city's history, and as the site where in 1803 Louisiana was made United States territory pursuant to the Louisiana Purchase.</p>" +
-                '<a href=""><button type="button">More Info</button></a>' + 
+                '<a href="NewOrleans/jacksonSquare.html"><button type="button">More Info</button></a>' + 
                 "</div>" +
                 "</div>";
 
@@ -258,7 +258,7 @@ const LSContent =
                 '<div id="bodyContent">' +
                 "<p>Lakeside Shopping Center, or simply Lakeside, is a shopping mall located at 3301 Veterans Memorial Boulevard in the New Orleans suburb of Metairie, Louisiana, United States." + 
                 "  It opened on March 24, 1960 as the first regional shopping mall in New Orleans and is the largest and busiest mall in Greater New Orleans.</p>" +
-                '<a href="#"><button type="button">More Info</button></a>' + 
+                '<a href="NewOrleans/lakeSide.html"><button type="button">More Info</button></a>' + 
                 "</div>" +
                 "</div>";
 
@@ -291,7 +291,7 @@ const CBContent =
                 '<div id="bodyContent">' +
                 "<p>Jean Lafitte National Historical Park and Preserve (French: Parc historique national et réserve Jean Lafitte) protects the natural and cultural resources of Louisiana's Mississippi River Delta region." + 
                 " It is named after French pirate Jean Lafitte and consists of six separate sites and a park headquarters</p>" +
-                '<a href="#"><button type="button">More Info</button></a>' + 
+                '<a href="NewOrleans/chalmetteBattlefield.html"><button type="button">More Info</button></a>' + 
                 "</div>" +
                 "</div>";
 
@@ -324,7 +324,7 @@ const CBContent =
                 '<div id="bodyContent">' +
                 "<p>The Caesars Superdome, commonly known as the Superdome (formerly known as Mercedes-Benz Superdome and Louisiana Superdome)," + 
                 " is a multi-purpose stadium located in the Central Business District of New Orleans, Louisiana. It is the home stadium of the New Orleans Saints of the National Football League</p>" +
-                '<a href="#"><button type="button">More Info</button></a>' + 
+                '<a href="NewOrleans/caesarsSuperdome.html"><button type="button">More Info</button></a>' + 
                 "</div>" +
                 "</div>";
 
@@ -357,7 +357,7 @@ const CBContent =
                 '<div id="bodyContent">' +
                 "<p>The National WWII Museum, formerly known as The National D-Day Museum, is a military history museum located in the Central Business District of New Orleans, Louisiana," + 
                 " U.S., on Andrew Higgins Drive between Camp Street and Magazine Street. The museum focuses on the contribution made by the United States to Allied victory in World War II.</p>" +
-                '<a href="#"><button type="button">More Info</button></a>' + 
+                '<a href="NewOrleans/ww2Museum.html"><button type="button">More Info</button></a>' + 
                 "</div>" +
                 "</div>";
 
@@ -390,7 +390,7 @@ const CBContent =
                 '<div id="bodyContent">' +
                 "<p>Mardi Gras World (also known as Blaine Kern's Mardi Gras World, MGW) is a tourist attraction located in New Orleans." + 
                 "  Guests tour the 300,000 square foot working warehouse where floats are made for Mardi Gras parades in New Orleans.</p>" +
-                '<a href="#"><button type="button">More Info</button></a>' + 
+                '<a href="NewOrleans/mardiGrasWorld.html"><button type="button">More Info</button></a>' + 
                 "</div>" +
                 "</div>";
 
